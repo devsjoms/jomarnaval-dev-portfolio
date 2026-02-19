0a6b30261f0d6c7565bdeb01e5d200429ad8cd46
jomsGutHib <jomarnaval023@gmail.com>
2026-02-20 00:15:37 +0800

    commmtttxxxx

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const navs = document.getElementsByClassName("nav-link");
 const myName = document.getElementById("introduce");
+const exit = document.getElementById("x-btn");
+const popUp = document.getElementById("msg-box");
 
+const body = document.body;
+
+exit.addEventListener('click',()=>{
+  popUp.style.display = "none";
+  body.classList.remove('no-scroll')
+})
+function notReady(){
+  popUp.style.display = "block";
+  body.classList.add('no-scroll')
+}
 function flexNav() {
   navLinks.forEach(link => {
     link.classList.toggle('open');
   });
 }
-
-function notReady(){
-  alert("not available for now.")
-}
-
 setTimeout(()=>{
   myName.innerHTML = "Naval"
 },2000);
