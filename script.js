543bd6464157e34869094248c1f64cec6c27ee18
jomsGutHib <jomarnaval023@gmail.com>
2026-02-27 12:04:43 +0800

    commmxxxxx

--- script.js
+++ script.js
@@ -26,3 +26,31 @@ setTimeout(()=>{
 setTimeout(()=>{
   myName.innerHTML = "Jomar."
 },4000);
+
+
+(function () {
+    emailjs.init("1fxDC0joc34IySljE");
+})();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Stop page reload
+        emailjs.sendForm(
+            "service_ny3v9nx",
+            "template_vyz5nmg",
+            this
+        ).then(function () {
+            alert("Message sent successfully!");
+        }).catch(function (error) {
+            alert("Failed to send message.");
+            console.error(error);
+        });
+
+    });
+});
+
+const dict = {
+  "name": "jomar"
+}
+console.log(dict["name"])
