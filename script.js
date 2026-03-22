e1a159b99141509e36c5ce8d10535f9320ab835e
jomsGutHib <jomarnaval023@gmail.com>
2026-03-22 16:15:13 +0800

    commmxxxx

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ setTimeout(()=>{
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contact-form");
   form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Stop page reload
+        event.preventDefault();
         emailjs.sendForm(
             "service_ny3v9nx",
             "template_vyz5nmg",
@@ -50,7 +50,44 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-const dict = {
-  "name": "jomar"
+window.addEventListener("scroll",() =>{
+  const isScrolled = document.getElementsByClassName("navs")[0];
+  if(window.scrollY > 0){
+    isScrolled.classList.add("scrolled");
+  }
+  else{
+    isScrolled.classList.remove("scrolled");
+  }
+});
+
+const certImages = [
+  "Certificates/scientific_computing_FCC.png",
+  "Certificates/ccna_CN1.jpeg",
+  "Certificates/softEngr.png",
+  "Certificates/hrPyBasic.png",
+  "Certificates/py.png",
+  "Certificates/pyinter.jpg",
+  "Certificates/pydev.jpg",
+  "Certificates/java.jpg",
+  "Certificates/cpp.png",
+  "Certificates/ml.jpg",
+  "Certificates/DA_With_AI.jpg"
+];
+
+const cert = document.getElementById("crt")
+let currInd = 0;
+
+function prevImg(){
+  if(currInd === 0){
+    currInd = certImages.length -1
+  }
+  currInd--;
+  cert.src = certImages[currInd]
 }
-console.log(dict["name"])
+function nextImg(){
+  currInd++;
+  if(currInd === certImages.length){
+    currInd = 0
+  }
+  cert.src = certImages[currInd]
+}
